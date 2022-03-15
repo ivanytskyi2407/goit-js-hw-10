da378fdcd3cfa2387ab97d842cae7636d628dde1
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-03-15 20:17:43 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -11,7 +11,6 @@
 
 
 // *axios
-
 import axios from 'axios';
 export function fetchCountries(name) {
   return axios.get(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
